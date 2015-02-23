ce2254992feb91a06981636c74dad8e5fc958191
Keith Reynolds <strange.aspiration@gmail.com>
2015-02-23 20:18:53 +0000

    added more functions

--- app/actions.rb
+++ app/actions.rb
@@ -3,12 +3,33 @@ get '/' do
   erb :index
 end
 
-get '/songs/' do
+get '/songs' do
 	@songs = Song.all
   erb :"songs/index"
 end
 
 get '/songs/new' do
   erb :"songs/new"
-  redirect '/songs/'
+end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    author:  params[:author]
+  )
+  if @song.save
+    redirect '/songs'
+  else
+    erb :'songs/new'
+  end
+end
+
+get '/songs/:id' do
+  @other = []
+  @song = Song.find params[:id]
+  author1 = @song.author
+  Song.where(author: author1).each do |record|
+    @other << record.title
+  end
+  erb :'songs/details'
 end
